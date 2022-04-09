a199c491d72373ac715378ceaafeda4afb909a2c
Caio Cardoso <cmmcardoso@gmail.com>
2022-04-09 13:48:41 -0300

    add transnmitter control

--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,9 @@ RH_RF95 rf95;
 SoftwareSerial gps_ss(11,12); // arduino's (Rx, Tx)
 
 uint32_t pkt_sent = 0;
-uint32_t pkt_received = 0;
 union {
-   float dFloat[4];            // 4 bytes
-   uint32_t dBytes[4];   //  mapped onto the same storage as myFloat
+   float dFloat[2];            // 4 bytes
+   uint32_t dBytes[2];   //  mapped onto the same storage as myFloat
 } posicao;
 
 static void smartDelay(unsigned long ms)
@@ -48,50 +47,38 @@ void setup() {
 }
 
 void loop() {
-  if (rf95.available())
-  {
-   uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-   uint8_t len = sizeof(buf);
-   if (rf95.recv(buf, &len))
-   {
-     pkt_received += 1;
-     Serial.print("got request: ");
 
-     posicao.dBytes[0] = (((uint32_t)(buf[0] & 0xff)) << 24) | (((uint32_t)(buf[1] & 0xff)) << 16) | (((uint32_t)(buf[2] & 0xff)) << 8) | (((uint32_t)(buf[3] & 0xff)));
-     posicao.dBytes[1] = (((uint32_t)(buf[4] & 0xff)) << 24) | (((uint32_t)(buf[5] & 0xff)) << 16) | (((uint32_t)(buf[6] & 0xff)) << 8) | (((uint32_t)(buf[7] & 0xff)));
-     pkt_sent = (((uint32_t)(buf[8] & 0xff)) << 24) | (((uint32_t)(buf[9] & 0xff)) << 16) | (((uint32_t)(buf[10] & 0xff)) << 8) | (((uint32_t)(buf[11] & 0xff)));
-
-     posicao.dFloat[2] = gps.location.lat();
-     posicao.dFloat[3] = gps.location.lng();
-
-     Serial.print(posicao.dFloat[2],5);
-     Serial.print(" ");
-     Serial.print(posicao.dFloat[3],5);
-     Serial.print(SF);
-     Serial.print(" ");
-     Serial.print(BANDWIDTH);
-     Serial.print(" ");
-     Serial.print(rf95.lastRssi());
-     Serial.print(" ");
-     Serial.print(rf95.lastSNR());
-     Serial.print(" ");
-     Serial.print(posicao.dFloat[0],5);
-     Serial.print(" ");
-     Serial.print(posicao.dFloat[1],5);
-     Serial.print(" ");
-     Serial.print(pkt_sent);
-     Serial.print(pkt_received);
-
-     Serial.println();
-   }
-   else
-   {
-     Serial.println("recv failed");
-   }
-  }
-
+  // update gps information
   smartDelay(10);
   
   if (millis() > 5000 && gps.charsProcessed() < 10)
     Serial.println(F("No GPS data received: check wiring"));
+
+  // prepare packet to be delivered
+  posicao.dFloat[0] = gps.location.lat();
+  posicao.dFloat[1] = gps.location.lng();
+  
+  uint8_t payload[12];
+
+  //Latitude do Car
+  for (int i = 0; i < 4; i++)
+  {
+    payload[i] = ((posicao.dBytes[0]) >> (24 - ( 8 * i )) & 0xff);
+  }
+
+  //Longitude Car
+  for (int i = 4; i < 8; i++)
+  {
+    payload[i] = ((posicao.dBytes[1]) >> (24 - ( 8 * (i - 4) )) & 0xff);
+  }
+
+  //packet sent
+  for (int i = 8; i < 12; i++)
+  {
+    payload[i] = (pkt_sent >> (24 - ( 8 * (i - 8) )) & 0xff);
+  }
+
+  rf95.send(payload, sizeof(payload));
+  rf95.waitPacketSent();
+  pkt_sent+=1;
 }
